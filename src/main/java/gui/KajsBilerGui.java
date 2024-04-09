bc2168aa19b134317924b1f026e9cfebae2dafec
FillerDK <fillerdk1234@gmail.com>
2024-04-09 16:36:55 +0200

    gui changes

--- src/main/java/gui/KajsBilerGui.java
+++ src/main/java/gui/KajsBilerGui.java
@@ -1,9 +1,12 @@
 package gui;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
+
+import java.awt.*;
 
 public class KajsBilerGui extends Application {
 
@@ -16,13 +19,24 @@ public class KajsBilerGui extends Application {
         Scene scene = new Scene(pane);
         stage.setScene(scene);
         stage.show();
+
+        scene.setOnMouseClicked(event -> {
+            if (!txfSøg.equals(event.getSource())) {
+                txfSøg.getParent().requestFocus();
+            }
+        });
     }
+
+    private TextField txfSøg = new TextField();
 
     // -------------------------------------------------------------------------
 
     private void initContent(GridPane pane) {
         // show or hide grid lines
         pane.setGridLinesVisible(true);
+        pane.setHgap(10);
+        pane.setVgap(10);
+        pane.setPadding(new Insets(10));
 
 
     }
