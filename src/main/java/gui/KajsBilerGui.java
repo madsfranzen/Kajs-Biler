ccb82db32d4a9a82998a83cf410dfc184be366e0
Mads Franzen <madsfranzen@live.dk>
2024-04-09 18:47:49 +0200

    Login system v0.01

--- src/main/java/gui/KajsBilerGui.java
+++ src/main/java/gui/KajsBilerGui.java
@@ -2,42 +2,93 @@ package gui;
 
 import javafx.application.Application;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
+import model.controller.Controller;
 
 public class KajsBilerGui extends Application {
 
+    Stage window;
+    Scene mainScene, loginScene;
+    GridPane mainPane = new GridPane();
+    GridPane loginPane = new GridPane();
+
+    // LOGIN TING
+    Label lblWrongPassword = new Label("Forkert brugernavn/kodeord");
+    private TextField txfUsername = new TextField();
+    private PasswordField pswPassword = new PasswordField();
     private TextField txfSøg = new TextField();
+    private Button btnLogin = new Button("Login");
+    private Controller controller = new Controller();
 
     @Override
     public void start(Stage stage) {
+        window = stage;
         stage.setTitle("Kajs Biler");
-        GridPane pane = new GridPane();
-        this.initContent(pane);
 
-        Scene scene = new Scene(pane);
-        stage.setScene(scene);
-        stage.show();
+        Scene mainScene = new Scene(mainPane);
+        Scene loginScene = new Scene(loginPane);
 
-        scene.setOnMouseClicked(event -> {
+        mainScene.setOnMouseClicked(event -> {
             if (!txfSøg.equals(event.getSource())) {
                 txfSøg.getParent().requestFocus();
             }
         });
+
+        this.initContent(mainPane, loginPane);
+        stage.setScene(loginScene);
+        stage.show();
+
+        btnLogin.setOnMouseClicked(event -> btnLoginAction());
     }
 
-    // -------------------------------------------------------------------------
+    // --------------------------- M A I N - W I N D O W ---------------------------------
 
-    private void initContent(GridPane pane) {
-        // show or hide grid lines
-        pane.setGridLinesVisible(true);
-        pane.setHgap(10);
-        pane.setVgap(10);
-        pane.setPadding(new Insets(10));
+    private void initContent(GridPane mainPane, GridPane loginPane) {
 
-        pane.add(txfSøg, 0, 0);
+        mainPane.setGridLinesVisible(true);
+        mainPane.setHgap(10);
+        mainPane.setVgap(10);
+        mainPane.setPadding(new Insets(10));
 
+        Label lblHello = new Label("Hello");
+        mainPane.add(txfSøg, 0, 0);
+        mainPane.add(lblHello, 0, 1);
+        mainPane.setAlignment(Pos.CENTER);
+
+        // ----------------------------- L O G I N ----------------------------------------
+
+        loginPane.setGridLinesVisible(false);
+        loginPane.setHgap(10);
+        loginPane.setVgap(10);
+        loginPane.setPadding(new Insets(10));
+        loginPane.setMinWidth(250);
+        loginPane.setMinHeight(250);
+        loginPane.setAlignment(Pos.CENTER);
+
+        loginPane.add(txfUsername, 0, 0);
+        loginPane.add(pswPassword, 0, 1);
+        HBox btnBox = new HBox(btnLogin);
+        loginPane.add(btnBox, 0, 3);
+        btnBox.setAlignment(Pos.CENTER);
+    }
+
+
+    private void btnLoginAction() {
+        // Checks for correct username/password and changes scene if correct
+        if (controller.checkLogin(txfUsername.getText(), pswPassword.getText())) {
+            window.setScene(mainScene);
+        } else {
+            loginPane.add(lblWrongPassword, 0, 2);
+            txfUsername.setText("");
+            pswPassword.setText("");
+        }
     }
 }
--- src/main/java/model/controller/Controller.java
+++ src/main/java/model/controller/Controller.java
@@ -1,6 +1,17 @@
 package model.controller;
 
+import gui.KajsBilerGui;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 public class Controller {
+
+    public static boolean checkLogin(String username, String password) {
+        if (username.equals("Mads") && password.equals("1234")) {
+            return true;
+        } else return false;
+    }
+
     public void opretKunde() {
 
     }
@@ -8,4 +19,5 @@ public class Controller {
     public void opretPrisgruppe() {
 
     }
+
 }
