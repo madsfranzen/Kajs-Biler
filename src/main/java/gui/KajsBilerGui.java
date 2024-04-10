4b9dcf161ef565323ea35775b584bc6b416299a5
FillerDK <fillerdk1234@gmail.com>
2024-04-10 09:50:33 +0200

    Scene change update

--- src/main/java/gui/KajsBilerGui.java
+++ src/main/java/gui/KajsBilerGui.java
@@ -50,9 +50,7 @@ public class KajsBilerGui extends Application {
             if (controller.checkLogin(txfUsername.getText(), pswPassword.getText())) {
                 window.setScene(mainScene);
             } else {
-                loginPane.add(lblWrongPassword, 0, 2);
-                txfUsername.setText("");
-                pswPassword.setText("");
+                btnLoginAction();
             }
         });
     }
@@ -91,12 +89,8 @@ public class KajsBilerGui extends Application {
 
     private void btnLoginAction() {
         // Checks for correct username/password and changes scene if correct
-        if (controller.checkLogin(txfUsername.getText(), pswPassword.getText())) {
-            window.setScene(mainScene);
-        } else {
-            loginPane.add(lblWrongPassword, 0, 2);
-            txfUsername.setText("");
-            pswPassword.setText("");
-        }
+        loginPane.add(lblWrongPassword, 0, 2);
+        txfUsername.setText("");
+        pswPassword.setText("");
     }
 }
